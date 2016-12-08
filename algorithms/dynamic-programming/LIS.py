34169a96705730ea75fe23b31bfd558b37cc09b1
Sunil <sunhick@gmail.com>
2016-12-07 17:01:51 -0700

    added LIS with n*log(n) solution

---
+++ algorithms/dynamic-programming/LIS.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+import bisect
+
+ar = [10, 22, 9, 33, 21, 50, 41, 60, 80]
+
+s = []
+l = 0
+
+# N*log(N) for LIS
+for e in ar:
+    i = bisect.bisect_left(s, e)
+    if (i == l):
+        s.append(e)
+        l += 1
+    elif (i >=0 and i <= l):
+        s[i] = e
+    else:
+        s.append(e)
+        l += 1
+
+print("LIS =", l)
