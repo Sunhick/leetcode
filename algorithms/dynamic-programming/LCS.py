2e90abc2e9354f272ff748e67179bee6e8c8f1f0
Sunil <sunhick@gmail.com>
2016-12-08 19:59:50 -0700

    solved lcs

--- algorithms/dynamic-programming/LCS.py
+++ algorithms/dynamic-programming/LCS.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
 
+import numpy as np
+
 str1 = "AGGTAB"
 str2 = "GXTXAYB"
 
+m, n = len(str1), len(str2)
+
+dp = np.zeros((m+1, n+1), dtype=int)
+
+for  i in range(1, m+1):
+    for j in range(1, n+1):
+        dp[i, j] = (dp[i-1, j-1]  + 1) if str1[i-1] == str2[j-1] else (max(dp[i-1, j], dp[i, j-1]))
+
+print("LCS: \n", dp)
+print("LCS =", dp[m, n])
