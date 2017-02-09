e9d7da834948c57e2f3e1b8261538e72d6d78ad4
Sunil <sunhick@gmail.com>
2017-02-08 17:27:15 -0700

    solved countet game bit algorithms

--- algorithms/random/CounterGame.py
+++ algorithms/random/CounterGame.py
@@ -8,11 +8,11 @@ def WhoWins(N):
             turn += 1
         else:
             largest = 1 #largest power of 2
-            while(largest<<1 < n):
+            while(largest<<1 < N):
                 largest = largest << 1
             N -= largest
             turn += 1
-    return "Richard" if c%2==0 else "Louise"
+    return "Richard" if turn%2==0 else "Louise"
 
 def main():
     n = int(input())
