6f3f6958b5f6d0308cba3be27f599a1465ddc2f9
Sunil <sunhick@gmail.com>
2017-02-08 17:26:07 -0700

    solved countet game bit algorithms

---
+++ algorithms/random/CounterGame.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+def WhoWins(N):
+    turn = 0 
+    while(N != 1):
+        if N % 2 == 0:
+            N = N >> 1
+            turn += 1
+        else:
+            largest = 1 #largest power of 2
+            while(largest<<1 < n):
+                largest = largest << 1
+            N -= largest
+            turn += 1
+    return "Richard" if c%2==0 else "Louise"
+
+def main():
+    n = int(input())
+    for i in range(n):
+        N  = int(input())
+        name = WhoWins(N)
+        print(name)
+
+if __name__ == '__main__':
+    main()
